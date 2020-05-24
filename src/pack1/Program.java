c8e85a51387cfa26fb2771cc8452e59d48d1f204
Miroso02 <vaniastronov@gmail.com>
2020-05-24 21:50:35 +0300

    First commit

---
+++ src/pack1/Program.java
@@ -0,0 +1,12 @@
+package pack1;
+import processing.core.PApplet;
+
+public class Program extends PApplet
+{
+	public static void main(String[] args) 
+	{
+		Program sketch = new Program();
+		String[] pargs = {""};
+		PApplet.runSketch(pargs, sketch);
+	}
+}
