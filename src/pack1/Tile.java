f1c0a0e6877937465c4e8c58ce1ddb63f78f6d1e
Miroso02 <vaniastronov@gmail.com>
2020-06-08 21:00:12 +0300

    Added smooth drawing

--- src/pack1/Tile.java
+++ src/pack1/Tile.java
@@ -1,12 +1,14 @@
 package pack1;
 
+import java.util.ArrayList;
 import processing.core.PApplet;
 
 public class Tile
-{
+{	
 	Vector position;
-	boolean active;
+	float activation;
 	
+	private ArrayList<Tile> adjacent = new ArrayList<Tile>();
 	private PApplet sketch;
 	private static final int TILE_SIZE = Program.TILE_SIZE;
 	
@@ -14,12 +16,12 @@ public class Tile
 	{
 		this.sketch = Program.sketch;
 		position = new Vector(xPos * TILE_SIZE, yPos * TILE_SIZE);
-		active = false;
+		activation = 0;
 	}
 	
 	public void display()
 	{
-		sketch.fill(active ? 0 : 255);
+		sketch.fill(255 * (1 - activation));
 		Program.rect(position, TILE_SIZE);
 	}
 	
@@ -29,7 +31,14 @@ public class Tile
 		{
 			if (mouseOn())
 			{
-				this.active = true;
+				activation += 0.6;
+				if (activation > 1) activation = 1; 
+				for (Tile tile : adjacent)
+				{
+					tile.activation += 0.15;
+					if (tile.activation > 1)
+						tile.activation = 1;
+				}
 			}
 		}
 	}
@@ -38,5 +47,15 @@ public class Tile
 	{
 		return (int) Math.abs(sketch.mouseX - TILE_SIZE / 2 - position.x) < TILE_SIZE / 2 
 			&& (int) Math.abs(sketch.mouseY - TILE_SIZE / 2 - position.y) < TILE_SIZE / 2;
+	}
+	
+	public boolean addAdj(Tile adj)
+	{
+		if (adj != null) 
+		{
+			adjacent.add(adj);
+			return true;
+		}
+		return false;
 	}
 }
--- src/pack1/TileController.java
+++ src/pack1/TileController.java
@@ -4,14 +4,33 @@ public class TileController
 {
 	Tile[] tiles;
 	final int tilesNumber;
+	final int W_IN_TILES;
+	final int H_IN_TILES;
 	
 	public TileController(int tilesNumber)
 	{
+		this.W_IN_TILES = Program.W_IN_TILES;
+		this.H_IN_TILES = Program.H_IN_TILES;
 		this.tilesNumber = tilesNumber;
+		
 		tiles = new Tile[tilesNumber];
 		for (int i = 0; i < tilesNumber; i++)
 		{
-			tiles[i] = new Tile(i % Program.W_IN_TILES, (int) (i / Program.W_IN_TILES));
+			int xPos = i % W_IN_TILES;
+			int yPos = (int) (i / W_IN_TILES);
+			tiles[i] = new Tile(xPos, yPos);
+		}
+		
+		for (int i = 0; i < W_IN_TILES; i++)
+		{
+			for (int j = 0; j < H_IN_TILES; j++)
+			{
+				Tile tile = getTile(i, j);
+				tile.addAdj(getTile(i - 1, j));
+				tile.addAdj(getTile(i + 1, j));
+				tile.addAdj(getTile(i, j - 1));
+				tile.addAdj(getTile(i, j + 1));
+			}
 		}
 	}
 	
@@ -28,7 +47,7 @@ public class TileController
 	{
 		for (Tile tile : tiles)
 		{
-			tile.active = false;
+			tile.activation = 0;
 		}
 	}
 	
@@ -37,7 +56,7 @@ public class TileController
 		float[] data = new float[tilesNumber];
 		for (int i = 0; i < tilesNumber; i++)
 		{
-			data[i] = tiles[i].active ? 1 : 0; 
+			data[i] = tiles[i].activation; 
 		}
 		return data;
 	}
@@ -45,8 +64,15 @@ public class TileController
 	public boolean isAnyActive()
 	{
 		for (Tile t : tiles)
-			if (t.active)
+			if (t.activation > 0)
 				return true;
 		return false;
+	}
+	
+	public Tile getTile(int xPos, int yPos) 
+	{	
+		if (xPos < 0 || xPos >= W_IN_TILES || yPos < 0 || yPos >= H_IN_TILES)
+			return null;
+		return tiles[xPos + yPos * W_IN_TILES];
 	}
 }
